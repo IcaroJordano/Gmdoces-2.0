72ae5de7a09f40179b7f36877168448c3a386a60
Icaro Jordano <Icarojordano>
2024-10-20 04:36:59 -0300

    feat(Header): create Header  mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { CardPreview } from "./CardPreview"
+import { Header } from "./Header"
 
 function App() {
   return (
     <div className="h-screen">
       <CardPreview></CardPreview>
+      <Header></Header>
     </div>
   )
 }
